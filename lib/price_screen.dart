9603f82b5398e0d93ed93a69a7d53af60e84b144
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2021-02-19 22:06:31 +0600

    completed exchage rate app for crypto currency

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:loading/loading.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,86 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = currenciesList.first;
+  Map<String, String> bitcoinRate = {};
+
+  DropdownButton<String> androidDropDown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+    }
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.white),
+      onChanged: (String newValue) {
+        setState(() {
+          selectedCurrency = newValue;
+          exchangeRate();
+        });
+      },
+      items: dropdownItems,
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedValue) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedValue];
+          exchangeRate();
+        });
+      },
+      children: pickerItems,
+    );
+  }
+
+  bool isWaiting = false;
+
+  void exchangeRate() async {
+    isWaiting = true;
+    try {
+      var coinData = CoinData();
+      var rate = await coinData.getCoinData(selectedCurrency);
+      isWaiting = false;
+      setState(() {
+        bitcoinRate = rate;
+      });
+    } catch (e) {
+      print('Exception: $e');
+    }
+  }
+
+  Column makeCards() {
+    List<ReuseableCard> cryptoCards = [];
+    for (String crypto in cryptoList) {
+      cryptoCards.add(
+        ReuseableCard(
+          cryptoCurrency: crypto,
+          selectedCurrency: selectedCurrency,
+          bitcoinRate: isWaiting ? '?' : bitcoinRate[crypto],
+        ),
+      );
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: cryptoCards,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,35 +100,54 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Container(
+            child: makeCards(),
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? iOSPicker() : androidDropDown(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ReuseableCard extends StatelessWidget {
+  const ReuseableCard({
+    @required this.cryptoCurrency,
+    @required this.bitcoinRate,
+    @required this.selectedCurrency,
+  });
+
+  final String cryptoCurrency;
+  final String bitcoinRate;
+  final String selectedCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+          child: Text(
+            '1 $cryptoCurrency = $bitcoinRate $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
